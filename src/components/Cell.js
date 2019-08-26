709d8c474b3b4f6033f27439ff3ba3a4e7b09a8a
Audrey Moon <audrey@loveme.computer>
2019-08-25 19:58:21 -0500

    checking in & prepping to move repo to Glitch

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-class Cell extends React.Component {
-  render() {
-    return (
-      <div className="cell" onClick={this.handleClickCell}>
-        {/* &nbsp; (y:{this.props.y}, x:{this.props.x} */}
-        {/* NOTE: rendering mine glyph based on this.props.mine (boolean) */}
-      </div>
-    )
-  }
+function Cell() {
+  return (
+    <div className="cell">
+      &nbsp;
+      {/* NOTE: rendering mine glyph based on this.props.mine (boolean) */}
+    </div>
+  )
 }
 
 export default Cell
